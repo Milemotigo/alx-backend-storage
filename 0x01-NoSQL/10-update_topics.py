f7ea639b6f173047c7047a7c7092338241a1c083
milemotigo <mtigo@gmail.com>
2023-10-17 17:47:08 +0100

    new commit

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''10-update_topics.py
+'''
+
+def update_topics(mongo_collection, name, topics):
+    '''
+        Python function that changes all topics of a school document based on the name
+    '''
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
