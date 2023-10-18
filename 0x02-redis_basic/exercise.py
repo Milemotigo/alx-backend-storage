39aef17ba39dddd4aa57f8d983bd2617d6735ac5
milemotigo <mtigo@gmail.com>
2023-10-19 00:24:32 +0100

    new commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,10 +3,24 @@
     Cache class. In the __init__ method, store an instance of the Redis client as a private
     variable named _redis (using redis.Redis()) and flush the instance using flushdb
 '''
-
+from functools import wraps
 import redis
 from uuid import uuid4
-from typing import Union, Callable
+from typing import Union, Callable, Any
+
+def count_calls(method: Callable) -> Callable:
+    '''Tracks the number of calls made to a method in a Cache class.
+    '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        '''returns the given method after incrementing its call counter.
+        '''
+
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 class Cache:
     '''Represents an object for storing data in a Redis data storage.
@@ -16,6 +30,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Store method that takes a data argument and returns a string
         '''
